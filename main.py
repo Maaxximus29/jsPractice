19908bd9fd7da9b7c535d3ad924d340fd8f91243
Pranav <pranavraipur03@gmail.com>
2026-03-15 22:01:33 +0530

    commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+import math
+u = int(input())
+for _ in range(u):
+    line = input().split()
+    x = int(line[0])
+    n = int(line[1])
+    pos = math.ceil(n/2)
+    negat = n - pos
+    print((x * pos)-(x*negat))
