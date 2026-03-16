e700ecb43d6f817b9a85a7f6f85786d1884c179e
Pranav <pranavraipur03@gmail.com>
2026-03-16 22:35:56 +0530

    commit

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ for _ in range(u):
     pos = math.ceil(n/2)
     negat = n - pos
     print((x * pos)-(x*negat))
+
+    #insight s - sum(n)then greediky add it in some combination to check if its possible or not so basically brute force it 
+    #removing which prefix gives me the answer and what happens if that prefix exists or does not exist in the hashmap
+    # well if it exists then add += 1 if it doesnt then just add that prefix? not sure wull seee tomorrow
+    #Problem 2193A codeforces dbmb and the array
+    # this does look nice to be fair 
